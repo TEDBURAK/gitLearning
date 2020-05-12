fd63ddc53078327ff4cf27bc0466219ca8324404
TARIKERKAM <tedogan@hotmail.com>
2020-05-12 18:46:32 +0400

    add Practice.java

--- src/tests/Winter.java
+++ src/tests/Winter.java
@@ -2,6 +2,6 @@ package tests;
 
 public class Winter {
     public static void main(String[] args) {
-        System.out.println("My secomd ");
+        System.out.println("My secomd chance");
     }
 }
